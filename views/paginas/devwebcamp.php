2e972b1c7328cfcf62e1a37ea3e31db2774d29e9
Randy Urriola <randy.urriolah@gmail.com>
2023-03-22 11:51:02 -0500

    Proyecto DevWebCamp

---
+++ views/paginas/devwebcamp.php
@@ -0,0 +1,19 @@
+<main class="devwebcamp">
+  <h2 class="devwebcamp__heading"><?php echo $titulo; ?></h2>
+  <p class="devwebcamp__descripcion">Conoce la conferencia más importante de Latinoamérica</p>
+
+  <div class="devwebcamp__grid">
+    <div class="devwebcamp__imagen">
+      <picture>
+        <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+        <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/i,g/sobre_devwebcamp.jpg" alt="Imagen DevWebcamp">
+      </picture>
+    </div>
+
+    <div class="devwebcamp__contenido">
+      <p class="devwebcamp__texto">Vivamus rutrum fringilla odio at pharetra. Vestibulum maximus lobortis efficitur. Maecenas efficitur ultrices faucibus. Mauris nec cursus ligula. Sed erat est, auctor vel consequat vitae, laoreet sit amet augue. Cras fringilla sagittis eleifend. Nullam pretium id mauris ut consectetur. Suspendisse lacinia libero at finibus semper.</p>
+      <p class="devwebcamp__texto">Suspendisse arcu lectus, interdum a imperdiet et, tincidunt et nibh. Sed ornare mauris eget nibh commodo egestas. Pellentesque vehicula, metus vitae ullamcorper dapibus, felis turpis pretium purus, ac scelerisque ex tortor non orci. In hac habitasse platea dictumst. Vestibulum semper nisl sit amet nunc venenatis commodo. Cras vel justo velit. Vivamus placerat, orci in tempus bibendum, erat ex lobortis sem, efficitur varius metus diam et dolor. Morbi at pretium nibh. Nunc sagittis, felis finibus malesuada molestie, nisl eros mollis nisi, vitae eleifend ex eros sed quam. Quisque ante massa, finibus vel tempus sit amet, aliquam in neque.</p>
+    </div>
+  </div>
+</main>
---
+++ views/registro/crear.php
@@ -0,0 +1,45 @@
+<main class="registro">
+  <h2 class="registro__heading"><?php echo $titulo; ?></h2>
+  <p class="registro__descripcion">Elige tu plan</p>
+
+  <div class="paquetes__grid">
+    <div class="paquete">
+      <h3 class="paquete__nombre">Pase Gratis</h3>
+      <ul class="paquete__lista">
+        <li class="paquete__elemento">Acceso Virtual a DebWebCamp</li>
+      </ul>
+
+      <p class="paquete__precio">$0</p>
+
+      <form action="/finalizar-registro/gratis" method="POST">
+        <input type="submit" value="Inscripción Gratis" class="paquetes__submit">
+      </form>
+    </div>
+
+    <div class="paquete">
+      <h3 class="paquete__nombre">Pase Presencial</h3>
+      <ul class="paquete__lista">
+        <li class="paquete__elemento">Acceso Presencial a DebWebCamp</li>
+        <li class="paquete__elemento">Pase por 2 días</li>
+        <li class="paquete__elemento">Acceso a talleres y conferencias</li>
+        <li class="paquete__elemento">Acceso a las grabaciones</li>
+        <li class="paquete__elemento">Camisa del Evento</li>
+        <li class="paquete__elemento">Comida y Bebida</li>
+      </ul>
+
+      <p class="paquete__precio">$199</p>
+    </div>
+
+    <div class="paquete">
+      <h3 class="paquete__nombre">Pase Virtual</h3>
+      <ul class="paquete__lista">
+        <li class="paquete__elemento">Acceso Virtual a DebWebCamp</li>
+        <li class="paquete__elemento">Pase por 2 días</li>
+        <li class="paquete__elemento">Acceso a talleres y conferencias</li>
+        <li class="paquete__elemento">Acceso a las grabaciones</li>
+      </ul>
+
+      <p class="paquete__precio">$49</p>
+    </div>
+  </div>
+</main>
